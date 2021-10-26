0016634b90fd43d0881d58382aeb3dc1c9677f32
SRCinVA <scitarella@hotmail.com>
2021-10-25 20:27:46 -0400

    First commit

---
+++ password_manager.py
@@ -0,0 +1,9 @@
+pwd = input("What is the master password? ")
+
+while True:
+    mode = input("Would ou like to add a new password or view existing ones ('view' or 'add') ")
+    if mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    print("Invalid answer.")
