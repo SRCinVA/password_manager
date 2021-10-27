2f229a5138e100b21ad17517e8bc5dc6104cd69b
SRCinVA <scitarella@hotmail.com>
2021-10-26 20:44:20 -0400

    more progress

--- password_manager.py
+++ password_manager.py
@@ -1,9 +1,31 @@
-pwd = input("What is the master password? ")
+master_pwd = input("What is the master password? ")
+
+def view():
+    pass # this just prevetns indentation errors
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+    
+    with open('passwords.txt', 'a') as f:   # 'with' opens the file, let's us complete tasks, and then closes that file automatically
+                                            # 'a' is 'append mode' (other are 'w' and 'r') 
+                                            # 'as f' names that file ('f')
+        f.write(name + "|" + pwd)
 
 while True:
-    mode = input("Would ou like to add a new password or view existing ones ('view' or 'add') ")
+    mode = input("Would you like to add a new password or view existing ones ('view' or 'add'). Press 'q' to quit: ").lower()
+    
+    if mode == "q":
+        print("Very well, we'll stop the program!")
+        break
+
     if mode == "view":
-        pass
+        view()
+    
     elif mode == "add":
-        pass
-    print("Invalid answer.")
+        add()
+    
+    else:
+        print("Invalid mode.")
+    
+    continue # to go back to the top of the while loop
