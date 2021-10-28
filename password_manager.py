18199e439fbcc76778d564008c9c142597a8c3ef
SRCinVA <scitarella@hotmail.com>
2021-10-27 20:51:20 -0400

    Progress on view()

--- password_manager.py
+++ password_manager.py
@@ -1,7 +1,11 @@
 master_pwd = input("What is the master password? ")
 
-def view():
-    pass # this just prevetns indentation errors
+def view():# 'pass' just prevents indentation errors
+    with open('passwords.txt', 'r') as f:   # 'with' opens the file, let's us complete tasks, and then closes that file automatically
+                                            # 'r' is 'read mode' (we don't want to change it) 
+                                            # 'as f' names that file ('f')
+        for line in f.readlines():          # notice that readlines() is a method
+            print(line.rstrip())            # oddly you have to 'rstrip' the carriage retun off of it.)
 
 def add():
     name = input("Account Name: ")
@@ -10,7 +14,7 @@ def add():
     with open('passwords.txt', 'a') as f:   # 'with' opens the file, let's us complete tasks, and then closes that file automatically
                                             # 'a' is 'append mode' (other are 'w' and 'r') 
                                             # 'as f' names that file ('f')
-        f.write(name + "|" + pwd)
+        f.write(name + "|" + pwd + '\n')    # add a line break
 
 while True:
     mode = input("Would you like to add a new password or view existing ones ('view' or 'add'). Press 'q' to quit: ").lower()
